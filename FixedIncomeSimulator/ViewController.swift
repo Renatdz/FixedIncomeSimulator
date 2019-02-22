75aa85a14e595ecb37c5385d6f29313c2eb47830
Renato Mendes <renato.mendes@accenture.com>
2019-02-22 15:53:03 -0300

    Fix swiftlint file error and correct files

--- FixedIncomeSimulator/ViewController.swift
+++ FixedIncomeSimulator/ViewController.swift
@@ -3,18 +3,12 @@
 //  FixedIncomeSimulator
 //
 //  Created by renato.mendes on 22/02/19.
-//  Copyright © 2019 Renato Mendes. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
