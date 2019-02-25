e48d466356d2c7ba07d3b04ae84b39fe1233bec0
Renato Mendes <renato.mendes@accenture.com>
2019-02-25 17:58:51 -0300

    Create the network layer

---
+++ FixedIncomeSimulator/Network/APIClient/RequestAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  RequestAPIClient.swift
+//  FixedIncomeSimulator
+//
+//  Created by renato.mendes on 25/02/19.
+//  Copyright © 2019 Renato Mendes. All rights reserved.
+//
+
+import Foundation
